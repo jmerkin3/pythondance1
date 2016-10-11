8e4adbfff08a9672d0c6cc99a3a3d8fceb7affd5
Joey Merkin <jmerkin@chadwickschool.org>
2016-10-11 08:57:01 -0700

    created our dance script file and set up stuff

---
+++ pythondancetry1.py
@@ -0,0 +1,20 @@
+#beginning dance project! yewww!
+from Myro import *
+init("sim")
+
+
+#defining all dance moves
+
+def sqside1(): #moving robot in big square
+    penDown()
+    wait(.5)
+    forward(1,8)
+    penUp()
+    turnBy(270)
+    
+def somethingelse():
+    
+    
+
+def 
+
