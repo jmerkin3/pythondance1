4238e56cdcd9b10530bafcbf32479f8fb94cd6af
Joey Merkin <jmerkin@chadwickschool.org>
2016-10-14 12:04:12 -0700

    added loops and structure to code

--- pythondancetry1.py
+++ pythondancetry1.py
@@ -2,19 +2,29 @@
 from Myro import *
 init("sim")
 
-
 #defining all dance moves
-
-def sqside1(): #moving robot in big square
-    penDown()
+def move1(): #first move of the dance
+    forward(3,1)
+    
+def move2(): #second move of the dance
     wait(.5)
-    forward(1,8)
-    penUp()
-    turnBy(270)
+    turnRight(3,1)
     
-def somethingelse():
-    
-    
+def move3(): #third move of dance
+    wait(1)
 
-def 
+#until base drop, run this x number of times
+#try to sync end of this up with base drop
+for w in range(1,18):
+    move1()
+    move2()
+    move3()
+
+#movement commands for base drop and maybe initsim
+#stuff
+
+#go back to loop or make new loop
+for y in range(0,5):
+    move1()
+
 
